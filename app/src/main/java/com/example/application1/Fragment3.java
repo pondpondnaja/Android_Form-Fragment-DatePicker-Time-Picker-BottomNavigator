d4f7af8a73889c631f4732c0ba7debb629744ccf
Titiwat <titiwatkuarkamphun@gmail.com>
2020-09-23 20:00:44 +0700

    Update optional change to camera 23/Sep/2020

--- app/src/main/java/com/example/application1/Fragment3.java
+++ app/src/main/java/com/example/application1/Fragment3.java
@@ -1,16 +1,107 @@
 package com.example.application1;
 
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraDevice;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.squareup.picasso.Picasso;
+
+import java.util.Objects;
+
+import static android.app.Activity.RESULT_OK;
+
 public class Fragment3 extends Fragment {
+
+    private static final int PERMISSION_CODE = 1000;
+    private static final int IMAGE_CAPTURE_CODE = 1001;
+    ImageView img_pre;
+    Button camera_call;
+    Uri image_uri;
+    CameraDevice cameraDevice;
+    Context context;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment3,container,false);
+        View view = inflater.inflate(R.layout.fragment3, container, false);
+
+        context = view.getContext();
+
+        img_pre = view.findViewById(R.id.ImagePreview);
+        camera_call = view.findViewById(R.id.camera_call);
+
+        camera_call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cameraClick();
+            }
+        });
+
         return view;
     }
+
+    private void cameraClick() {
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+            //show popup to request permissions
+            requestPermissions(permission, PERMISSION_CODE);
+        } else {
+            //permission already granted
+            openCamera();
+        }
+    }
+
+    private void openCamera() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera");
+        image_uri = Objects.requireNonNull(getActivity()).getApplicationContext().getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        //Camera intent
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //called when image was captured from camera
+        if (resultCode == RESULT_OK) {
+            if (requestCode == IMAGE_CAPTURE_CODE) {
+                //set the image captured to our ImageView
+                //img_pre.setImageURI(image_uri);
+                Picasso.get().load(image_uri).into(img_pre);
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //this method is called, when user presses Allow or Deny from Permission Request Popup
+        if (requestCode == PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //permission from popup was granted
+                openCamera();
+            } else {
+                //permission from popup was denied
+                Toast.makeText(context, "Permission denied...", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/application1/MainActivity.java
+++ app/src/main/java/com/example/application1/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.application1;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import androidx.fragment.app.Fragment;
-
 import android.os.Bundle;
 import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -26,22 +24,23 @@ public class MainActivity extends AppCompatActivity {
     private BottomNavigationView.OnNavigationItemSelectedListener nav_listener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(MenuItem item) {
-            Fragment selecteed = null;
+            Fragment selected = null;
 
             switch (item.getItemId()){
 
                 case R.id.menu1:
-                    selecteed = new Fragment1();
+                    selected = new Fragment1();
                     break;
                 case R.id.menu2:
-                    selecteed = new Fragment2();
+                    selected = new Fragment2();
                     break;
                 case R.id.menu3:
-                    selecteed = new Fragment3();
+                    selected = new Fragment3();
                     break;
             }
 
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selecteed).addToBackStack(null).commit();
+            assert selected != null;
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selected).addToBackStack(null).commit();
 
             return true;
         }
